48b0a17e9b72ffe11fa4276fb22c8bfad38fcb5d
shumik-vladyslav <shumik.vladyslav@gmail.com>
2020-07-22 11:49:40 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+// function fn1() {
+//     var onhover = document.getElementById('l-case');
+//     var casopacity = document.getElementById('case');
+//
+//     if (onhover.style.hover) {
+//         casopacity.style.opacity = 1
+//     }
+//
+// }
+
+// (function fn1() {
+//     var elem = document.getElementById('l-case');
+//
+//     elem.onmouseover = function () {
+//         if (this.style.opacity = "0") {
+//             this.style.opacity = "1"
+//         }
+//     };
+//
+// }) ();
